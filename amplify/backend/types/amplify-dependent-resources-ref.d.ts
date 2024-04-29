d65a02663e4348504048a73b25b7fda12aa15720
mzw86020424 <72555480+mzw86020424@users.noreply.github.com>
2024-04-29 01:27:42 +0000

    graphqlAPIを作成してamplifyプロジェクトにデプロイ

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyappfb5bf887": {
       "AppClientID": "string",
